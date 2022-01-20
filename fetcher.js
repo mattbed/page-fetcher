11c478b7577df5fe6a3f33ade1f143c1c8fecfdf
Matthew <m.bednarski@gmail.com>
2022-01-20 19:48:36 +0000

    Refactored. Still missing stretch exercises.

--- fetcher.js
+++ fetcher.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
-const fetcher = process.argv.slice(2);
-const url = fetcher[0];
-const localPath = fetcher[1];
-
-
+const input = process.argv.slice(2);
 const request = require('request');
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error, '\nstatusCode:', response && response.statusCode);
-  }
-  fs.writeFile(localPath, body, err => {
-    if (err) {
-      console.error(err);
-      return;
+
+
+const fetcher = (url, localPath) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('error:', error, '\nstatusCode:', response && response.statusCode);
     }
-    console.log(`Downloaded and saved ${body.length} bytes to ${localPath}.`);
+    fs.writeFile(localPath, body, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`Downloaded and saved ${body.length} bytes to ${localPath}.`);
+    });
   });
-});
+};
+
+fetcher(input[0], input[1]);
